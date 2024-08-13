7a100f7c521a5d7f4097e877168cbcfec4212adc
vishal <vishal88kh@gmail.com>
2024-08-13 21:43:27 +0530

    implemented add user method

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -2,6 +2,8 @@ package dao;
 
 import model.User;
 
+import java.sql.SQLException;
+
 public interface UserDAO {
-    void addUser(User user);
+    void addUser(User user) throws SQLException;
 }
--- src/dao/UserDAOImpl.java
+++ src/dao/UserDAOImpl.java
@@ -1,11 +1,23 @@
 package dao;
 
 import model.User;
+import util.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 // actual logic which interact with db
 public class UserDAOImpl implements UserDAO {
     @Override
-    public void addUser(User user) {
-
+    public void addUser(User user) throws SQLException {
+        String sql = "INSERT INTO users(username, password,email) VALUES(?,?,?)";
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, user.userName);
+            preparedStatement.setString(2, user.password);
+            preparedStatement.setString(3, user.email);
+            preparedStatement.executeUpdate();
+        }
     }
 }
