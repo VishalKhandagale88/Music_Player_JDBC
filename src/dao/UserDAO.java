0e87d838a3a6674cc8a1ae18a9a0e0ce8b992c0e
vishal <vishal88kh@gmail.com>
2024-08-13 20:49:16 +0530

    added UserDAO and impl addUser implementation pending

---
+++ src/dao/UserDAO.java
@@ -0,0 +1,7 @@
+package dao;
+
+import model.User;
+
+public interface UserDAO {
+    void addUser(User user);
+}
---
+++ src/dao/UserDAOImpl.java
@@ -0,0 +1,11 @@
+package dao;
+
+import model.User;
+
+// actual logic which interact with db
+public class UserDAOImpl implements UserDAO {
+    @Override
+    public void addUser(User user) {
+
+    }
+}
