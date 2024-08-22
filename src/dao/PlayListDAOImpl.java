1358e332c354e5fbf75946e19656ecfb0bfde3f7
vishal <vishal88kh@gmail.com>
2024-08-22 20:32:40 +0530

    added implementation method

--- src/dao/PlayListDAOImpl.java
+++ src/dao/PlayListDAOImpl.java
@@ -11,10 +11,10 @@ public class PlayListDAOImpl implements PlayListDAO {
     @Override
     public void createPlayList(Playlist playlist) throws SQLException {
         String sql = "INSERT INTO playlists (name, user_id) VALUES (?,?)";
-        Connection connection = DatabaseConnection.getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        preparedStatement.setString(1,playlist.getName());
-        preparedStatement.setInt(2,playlist.getUser().getUserId());
-        preparedStatement.executeUpdate();
+        try (Connection connection = DatabaseConnection.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, playlist.getName());
+            preparedStatement.setInt(2, playlist.getUser().getUserId());
+            preparedStatement.executeUpdate();
+        }
     }
 }
