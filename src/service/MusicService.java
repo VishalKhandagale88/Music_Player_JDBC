f351e63b24b3e298557d75c1a1ab74a29893db8c
vishal <vishal88kh@gmail.com>
2024-08-22 20:32:08 +0530

    added create playlist method and getUSerByName

--- src/service/MusicService.java
+++ src/service/MusicService.java
@@ -1,8 +1,10 @@
 package service;
 
+import dao.PlayListDAOImpl;
 import dao.SongDAOImpl;
 import dao.UserDAO;
 import dao.UserDAOImpl;
+import model.Playlist;
 import model.Song;
 import model.User;
 
@@ -13,6 +15,7 @@ public class MusicService {
 
     private UserDAOImpl userDAO = new UserDAOImpl();
     private SongDAOImpl songDAO = new SongDAOImpl();
+    private PlayListDAOImpl playListDAO = new PlayListDAOImpl();
 
     public void addUser(User user) throws SQLException {
         userDAO.addUser(user);
@@ -22,6 +25,12 @@ public class MusicService {
     }
     public List<Song> getAllSongs() throws SQLException{
         return songDAO.getAllSongs();
+    }
+    public void createPlaylist(Playlist playlist) throws SQLException {
+        playListDAO.createPlayList(playlist);
+    }
+    public User getUserByName(String name) throws SQLException {
+        return userDAO.getUserByName(name);
     }
 
 }
